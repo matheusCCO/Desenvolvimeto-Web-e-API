2c8207d67005d77eca8c8dd876ca4ad9e86e2ac5
MATHEUS MARQUES BANDEIRA <632220119@fspoa.br>
2022-09-08 10:34:51 -0300

    criei arquivos na aula 5

---
+++ Aula5_API/script.js
@@ -0,0 +1,18 @@
+function lerDados(){
+    var xHTTP = new XMLHttpRequest();
+
+    xHTTP.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var obj = JSON.parse(this.responseText);
+
+            var conteudo = "Nome"+ obj.nome +"<br>";
+
+            conteudo += "Conjuge:" + obj.cojuge.nome + "<br>";
+
+            document.getElementById("dados").innerHTML = conteudo;
+        }
+    };
+
+    xHTTP.open("GET", meuJSON.json,true);
+    xHTTP.send();
+}
